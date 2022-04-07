daacc90c7021db1ef81bc7516a922d17ab868f3c
TimofeiShorokhov <limjkeee@mail.ru>
2022-04-07 14:52:40 +0300

    Fourth task

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"sha256-sum/services"
 )
 
@@ -12,29 +13,41 @@ var (
 	filePath string
 	helpPath bool
 	dirPath  string
+	hashAlg  string
 )
 
 func init() {
 	flag.StringVar(&filePath, "p", "", "Hash through file path")
 	flag.StringVar(&dirPath, "d", "", "Hash of all files through directory path")
+	flag.StringVar(&hashAlg, "a", "", "md5, sha512, default: sha256")
 	flag.BoolVar(&helpPath, "h", false, "info")
 	flag.Parse()
 }
 
 func main() {
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			log.Printf("stopped by user %d", sig)
+			os.Exit(1)
+		}
+	}()
+
 	switch {
 	case helpPath:
 		flag.Usage = func() {
 			fmt.Fprintf(os.Stderr, "Help with commands %s:\nUse one of the following commands:\n", os.Args[0])
 			flag.VisitAll(func(f *flag.Flag) {
-				fmt.Fprintf(os.Stderr, " flag 	-%v \n  	%v\n", f.Name, f.Usage)
+				fmt.Fprintf(os.Stderr, " flag:	-%v \n 	%v\n", f.Name, f.Usage)
 			})
 		}
 		flag.Usage()
 	case len(filePath) > 0:
-		fmt.Println(services.HashOfFile(filePath))
+		fmt.Println(services.HashOfFile(filePath, hashAlg))
 	case len(dirPath) > 0:
-		services.CheckSum(dirPath)
+		services.CheckSum(dirPath, hashAlg)
 	default:
 		log.Println("Error with flag, use '-h' flag for help ")
 	}
--- services/utils.go
+++ services/utils.go
@@ -1,7 +1,10 @@
 package services
 
 import (
+	"context"
+	"crypto/md5"
 	"crypto/sha256"
+	"crypto/sha512"
 	"fmt"
 	"io"
 	"os"
@@ -10,7 +13,7 @@ import (
 	"sync"
 )
 
-func HashOfFile(path string) string {
+func HashOfFile(path string, hashAlg string) string {
 
 	file, err := os.Open(path)
 	if err != nil {
@@ -20,19 +23,31 @@ func HashOfFile(path string) string {
 
 	defer file.Close()
 
-	hash := sha256.New()
-	_, err = io.Copy(hash, file)
+	var checkSum interface{}
 
+	switch hashAlg {
+	case "md5":
+		hash := md5.New()
+		_, err = io.Copy(hash, file)
+		checkSum = hash.Sum(nil)
+	case "512":
+		hash := sha512.New()
+		_, err = io.Copy(hash, file)
+		checkSum = hash.Sum(nil)
+	default:
+		hash := sha256.New()
+		_, err = io.Copy(hash, file)
+		checkSum = hash.Sum(nil)
+	}
 	if err != nil {
 		errors.CheckErr(err)
 		return ""
 	}
-	res := fmt.Sprintf("%x %s", hash.Sum(nil), filepath.Base(path))
+	res := fmt.Sprintf("%x %s", checkSum, filepath.Base(path))
 	return res
 }
 
 func HashOfDir(path string, paths chan string) {
-
 	err := filepath.Walk(path,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
@@ -50,14 +65,14 @@ func HashOfDir(path string, paths chan string) {
 	close(paths)
 }
 
-func Worker(wg *sync.WaitGroup, jobs <-chan string, results chan<- string) {
+func Worker(wg *sync.WaitGroup, jobs <-chan string, results chan<- string, hashAlg string) {
 	defer wg.Done()
 	for j := range jobs {
-		results <- HashOfFile(j)
+		results <- HashOfFile(j, hashAlg)
 	}
 }
 
-func Result(results chan string) {
+func Result(ctx context.Context, results chan string) {
 
 	for {
 		select {
@@ -66,11 +81,23 @@ func Result(results chan string) {
 				return
 			}
 			fmt.Println(hash)
+		case <-ctx.Done():
+			fmt.Println("canceled by user")
+			os.Exit(1)
+			return
 		}
 	}
 }
 
-func CheckSum(path string) {
+func CheckSum(path string, hashAlg string) {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	go func() {
+		fmt.Scanln()
+		cancel()
+	}()
+
 	jobs := make(chan string)
 	results := make(chan string)
 
@@ -79,10 +106,10 @@ func CheckSum(path string) {
 		var wg sync.WaitGroup
 		for w := 1; w <= 10; w++ {
 			wg.Add(1)
-			go Worker(&wg, jobs, results)
+			go Worker(&wg, jobs, results, hashAlg)
 		}
 		defer close(results)
 		wg.Wait()
 	}()
-	Result(results)
+	Result(ctx, results)
 }
